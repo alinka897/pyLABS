365a5db85ed24e1a8b8927d5c879fe6bbdd89d3b
alinka897 <alina.agapova9@yandex.ru>
2025-02-23 19:59:47 +0300

    Del def in 18

--- 18.py
+++ 18.py
@@ -1,10 +1,8 @@
 #Заполнить словарь списками и вывести тот айтем, у которого сумма наименьшая
 import random
 
-def func():
-    d = {x: [random.randint(-100,100) for i in range(5)] for x in range(4)}
-    print(d)
-    for k,v in d.items():
-        if v==min(d.values(), key = sum):
-            return (k,v)
-print(func())
+d = {x: [random.randint(-100,100) for i in range(5)] for x in range(4)}
+print(d)
+for k,v in d.items():
+    if v == min(d.values(), key = sum):
+        print(k,v)
