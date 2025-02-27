f546793d95aebf4c92b1c0fa0e357211b08a697f
alinka897 <alina.agapova9@yandex.ru>
2025-02-27 23:02:58 +0300

    Added docstring 18

--- 18.py
+++ 18.py
@@ -1,8 +1,13 @@
-#Заполнить словарь списками и вывести тот айтем, у которого сумма наименьшая
+"""
+Set a dictionary with 4 items,
+value of each is a list with 5 integers.
+Print the item with minimal sum.
+"""
+
 import random
 
 d = {x: [random.randint(-100,100) for i in range(5)] for x in range(4)}
 print(d)
-for k,v in d.items():
+for k, v in d.items():
     if v == min(d.values(), key = sum):
-        print(k,v)
+        print(k, v)
