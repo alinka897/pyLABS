4036a24c3b05bf12a63c81a1a08c8bb7eb44c164
alinka897 <alina.agapova9@yandex.ru>
2025-02-27 22:25:41 +0300

    English and nessecery spaces added 4

--- 4/4.py
+++ 4/4.py
@@ -1,16 +1,21 @@
-#Заполнить два файла и вывести их содержимое в два столбца
+"""
+Fill 2 files with random numbers
+and print them in 2 columns 
+"""
 import random
-def func(n,m):
+
+def func(n, m):
     f1 = open(n, 'w')
     f2 = open(m, 'w')
     for line in range(10):
         a = str(random.randint(-50,50))
         b = str(random.randint(-50,50))
-        f1.write(a+'\n')
-        f2.write(b+'\n')
-        print(a+"\t"+b)
+        f1.write(a + '\n')
+        f2.write(b + '\n')
+        print(a + '\t' + b)
     f1.close()
     f2.close()
-f1 = input("Введите путь к первому файлу: ")
-f2 = input("Введите путь ко второму файлу: ")
-func(f1,f2)
+
+f1 = input("Enter the path to the first file: ")
+f2 = input("Enter the path to the second file: ")
+func(f1, f2)
