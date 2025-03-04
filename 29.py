460421305ce9f49146f4e146008158852dbe4868
alinka897 <alina.agapova9@yandex.ru>
2025-03-04 14:00:04 +0300

    29 v0.99

--- 29.py
+++ 29.py
@@ -1,13 +1,23 @@
 '''
-
+Засунуть вручную этот путь как список, кот состоит из
+подстрок разной длины, вывести их, найти самую минимальную
+по длине подстроку и дать индекс ее начала
 '''
 
 from collections import deque
 
 def get_min_index():
     path = "/usr/share/nvim/runtime"
-    sub_strings = deque(path.split('/'))
-    v = min(strings, key=len)
-    print(strings, v)
+    strings = deque(path.split('/'))
+    strings.popleft()
+    for i in strings:
+        print(i, end = ' ') if i != strings[-1] else print(i)
+    for i, v in enumerate(strings):
+        if v == min(strings, key=len):
+            print(i, v)
+    
+    min_val = min(strings, key=len)
+    print(path.find(min_val), min_val)
+    
 
 get_min_index()
